2e994dfe6e1b1550a1f1bbab4f77c8e17c9ccbd4
BrunoMagela <brunomagelaa@gmail.com>
2024-03-12 20:32:27 -0300

    create user

---
+++ user.js
@@ -0,0 +1,17 @@
+const user = {
+    name: "Magela",
+    city: "Maringá",
+    work: "Administrator",
+    hobbie: "Play Games",
+};
+
+console.log(
+    "Nome: ",
+    user.name,
+    "Cidade: ",
+    user.city,
+    "Work: ",
+    user.work,
+    "Hobbie: ",
+    user.hobbie
+);
