fb2e9f501ad5022ff6cc41ca66015c608091a98c
BrunoMagela <brunomagelaa@gmail.com>
2024-03-12 20:45:48 -0300

    ajuste no codigo js

--- numerosMaisVinte.js
+++ numerosMaisVinte.js
@@ -1,5 +1,6 @@
 const array = [9, 4, 5, 10, 2, 90, 8, 6, 33];
 const maioresQueVinte = array.filter(numero => numero > 20);
+const menoresQueVinte = array.filter(numero => numero < 20);
 console.log('Números maiores que 20:', maioresQueVinte);
-console.log('Números menores que 20:', maioresQueVinte);
+console.log('Números menores que 20:', menoresQueVinte);
 
