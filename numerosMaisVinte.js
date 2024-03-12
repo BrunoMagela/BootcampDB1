96deaf5aebb70b0eab38872824c18d10f699cbae
BrunoMagela <brunomagelaa@gmail.com>
2024-03-12 20:43:51 -0300

    Alteracao no js numerosMaisVinte, agora mostra também os numerosMenosVinte

---
+++ numerosMaisVinte.js
@@ -0,0 +1,5 @@
+const array = [9, 4, 5, 10, 2, 90, 8, 6, 33];
+const maioresQueVinte = array.filter(numero => numero > 20);
+console.log('Números maiores que 20:', maioresQueVinte);
+console.log('Números menores que 20:', maioresQueVinte);
+
